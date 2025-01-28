88262b9274abe72ea4ce43c92c675ed3bc7c45f4
Darkgiant24 <gregoire.cabioch@gmail.com>
2025-01-28 18:19:42 -0500

    change

--- include/Socket.h
+++ include/Socket.h
@@ -1,18 +1,48 @@
-//
-// Created by super on 28/01/2025.
-//
+
+
+#endif //SOCKET_H
+
 
 #ifndef SOCKET_H
 #define SOCKET_H
 
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
 class Socket {
-    public:
-        Socket(const char* ip, const char* port);
-        ~Socket();
-        Socket(const char*port); // server version
-        void sendToServer(const char* message);
-        void receiveFromServer(char* message);
-        void listen(char* message);
+public:
+    // Constructeur et destructeur
+    Socket(const char* ip, const char* port);
+    ~Socket();
+
+    // Méthodes de communication
+    void sendToServer(const char* message);
+    void receiveFromServer(char* recvbuf);
+    void listen(char* buffer);
+
+    // Méthodes pour définir les adresses
+    void setServerAddr(const char* ip, const char* port);
+    void setListenAddr(const char* port);
+
+    // Méthode pour commencer à écouter
+    void startListening();
+
+private:
+    // Sockets pour envoyer et écouter
+    int SendSocket;         // Socket pour l'envoi de données
+    int ListenSocket;       // Socket pour l'écoute des connexions
+
+    // Structures pour les adresses
+    struct sockaddr_in serverAddr;   // Adresse du serveur pour l'envoi
+    struct sockaddr_in sender_addr;  // Adresse de l'expéditeur pour la réception
+    struct sockaddr_in clientAddr;   // Adresse du client pour l'écoute
+    struct sockaddr* result;         // Pointeur générique pour sockaddr
+    socklen_t result_size;           // Taille de la structure sockaddr
+    socklen_t sender_addr_size;      // Taille de la structure de l'expéditeur
+    socklen_t clientAddrLen;         // Taille de la structure du client
+
+    // Constantes pour les ports
+    static const char* DEFAULT_PORT; // Port par défaut
 };
 
-#endif //SOCKET_H
+#endif // SOCKET_H
--- src/Socket_posix.cpp
+++ src/Socket_posix.cpp
@@ -1,128 +1,98 @@
-#include "Socket.h"
-#include <stdexcept>
-#include <cstring>
-#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <errno.h>
+#include <unistd.h>
+#include "Socket.h"
 
-#define DEFAULT_BUFLEN 512
+Socket::Socket(const char* ip, const char* port) {
+    // Initialisation des sockets
+    SendSocket = socket(AF_INET, SOCK_DGRAM, 0);  // Création du socket pour l'envoi
+    ListenSocket = socket(AF_INET, SOCK_DGRAM, 0);  // Création du socket pour l'écoute
 
-
-Socket::Socket(const char *ip, const char *port) {
-    // Créer un socket UDP
-    SendSocket = socket(AF_INET, SOCK_DGRAM, 0);
     if (SendSocket < 0) {
-        throw std::runtime_error("Socket creation failed with error: " + std::to_string(errno));
+        perror("Error creating SendSocket");
+        return;
     }
 
-    // Résoudre l'adresse du serveur
-    struct sockaddr_in serverAddr;
+    if (ListenSocket < 0) {
+        perror("Error creating ListenSocket");
+        return;
+    }
+
+    // Configuration des adresses IP et des ports
     memset(&serverAddr, 0, sizeof(serverAddr));
     serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(atoi(port));
-    if (inet_pton(AF_INET, ip, &serverAddr.sin_addr) <= 0) {
-        throw std::runtime_error("Invalid IP address: " + std::string(ip));
-    }
+    serverAddr.sin_port = htons(atoi(port));  // Utilisation du port passé en argument
+    serverAddr.sin_addr.s_addr = inet_addr(ip);  // Utilisation de l'adresse IP passée en argument
 
+    // Préparation de la structure sockaddr pour l'envoi
     result = reinterpret_cast<struct sockaddr*>(&serverAddr);
     result_size = sizeof(serverAddr);
 }
 
-void Socket::sendToServer(const char* message) {
-    // Envoyer un message au serveur
-    ssize_t iResult = sendto(SendSocket, message, strlen(message), 0, result, result_size);
-    if (iResult < 0) {
-        throw std::runtime_error("sendto failed with error: " + std::to_string(errno));
-    }
-
-    printf("Bytes Sent: %ld\n", iResult);
-    printf("Sent message: %s\n", message);
-}
-
-void Socket::receiveFromServer(char* message) {
-    char recvbuf[DEFAULT_BUFLEN];
-    socklen_t recvbuflen = DEFAULT_BUFLEN;
-
-    struct sockaddr_storage sender_addr;
-    socklen_t sender_addr_size = sizeof(sender_addr);
-
-    ssize_t iResult = recvfrom(SendSocket, recvbuf, recvbuflen, 0, reinterpret_cast<struct sockaddr*>(&sender_addr), &sender_addr_size);
-    if (iResult > 0) {
-        recvbuf[iResult] = '\0'; 
-        printf("Bytes received: %ld\n", iResult);
-        printf("Message received: %s\n", recvbuf);
-    } else if (iResult == 0) {
-        printf("Connection closed\n");
-    } else {
-        throw std::runtime_error("recvfrom failed with error: " + std::to_string(errno));
-    }
-}
-
-Socket::Socket(const char* port) {
-    
-    ListenSocket = socket(AF_INET, SOCK_DGRAM, 0);
-    if (ListenSocket < 0) {
-        throw std::runtime_error("Socket creation failed with error: " + std::to_string(errno));
-    }
-
-    
-    struct sockaddr_in serverAddr;
-    memset(&serverAddr, 0, sizeof(serverAddr));
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(atoi(port));
-    serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-
-    
-    int iResult = bind(ListenSocket, reinterpret_cast<struct sockaddr*>(&serverAddr), sizeof(serverAddr));
-    if (iResult < 0) {
-        throw std::runtime_error("Bind failed with error: " + std::to_string(errno));
-    }
-}
-
-void Socket::listen(char* message) {
-    char recvbuf[DEFAULT_BUFLEN];
-    socklen_t recvbuflen = DEFAULT_BUFLEN;
-    struct sockaddr_in clientAddr;
-    socklen_t clientAddrLen = sizeof(clientAddr);
-
-    
-    printf("Server is waiting for data on port %s...\n", DEFAULT_PORT);
-    while (true) {
-        printf("Waiting to receive data...\n");
-
-       
-        memset(&clientAddr, 0, clientAddrLen);
-        memset(recvbuf, 0, DEFAULT_BUFLEN);
-
-        
-        ssize_t iResult = recvfrom(ListenSocket, recvbuf, recvbuflen, 0, reinterpret_cast<struct sockaddr*>(&clientAddr), &clientAddrLen);
-        if (iResult < 0) {
-            printf("recvfrom failed with error: %d\n", errno);
-            break;
-        }
-
-        
-        printf("Received %ld bytes from client: %s\n", iResult, recvbuf);
-
-        
-        const char *response = "Message received by server!";
-        iResult = sendto(ListenSocket, response, strlen(response), 0, reinterpret_cast<struct sockaddr*>(&clientAddr), clientAddrLen);
-        if (iResult < 0) {
-            printf("sendto failed with error: %d\n", errno);
-            break;
-        }
-
-        printf("Response sent to client.\n");
-    }
-}
-
 Socket::~Socket() {
+    // Fermeture des sockets ouverts
     if (SendSocket >= 0) {
         close(SendSocket);
     }
     if (ListenSocket >= 0) {
         close(ListenSocket);
     }
+}
+
+void Socket::sendToServer(const char* message) {
+    ssize_t iResult = sendto(SendSocket, message, strlen(message), 0, result, result_size);
+    if (iResult < 0) {
+        perror("Error sending data");
+    }
+}
+
+void Socket::receiveFromServer(char* recvbuf) {
+    ssize_t iResult = recvfrom(SendSocket, recvbuf, 1024, 0, reinterpret_cast<struct sockaddr*>(&sender_addr), &sender_addr_size);
+    if (iResult < 0) {
+        perror("Error receiving data");
+    }
+}
+
+void Socket::listen(char* buffer) {
+    char recvbuf[1024];  // Buffeur de réception
+    ssize_t iResult;
+
+    // Attente de la réception de données sur le socket
+    iResult = recvfrom(ListenSocket, recvbuf, sizeof(recvbuf), 0, reinterpret_cast<struct sockaddr*>(&clientAddr), &clientAddrLen);
+    if (iResult < 0) {
+        perror("Error receiving data");
+    } else {
+        // Traitement des données reçues
+        printf("Received data: %s\n", recvbuf);
+    }
+}
+
+void Socket::setServerAddr(const char* ip, const char* port) {
+    memset(&serverAddr, 0, sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(atoi(port));
+    serverAddr.sin_addr.s_addr = inet_addr(ip);
+    result = reinterpret_cast<struct sockaddr*>(&serverAddr);
+    result_size = sizeof(serverAddr);
+}
+
+void Socket::setListenAddr(const char* port) {
+    memset(&clientAddr, 0, sizeof(clientAddr));
+    clientAddr.sin_family = AF_INET;
+    clientAddr.sin_port = htons(atoi(port));
+    clientAddr.sin_addr.s_addr = INADDR_ANY;  // Écoute sur toutes les interfaces
+}
+
+void Socket::startListening() {
+    int bindResult = bind(ListenSocket, reinterpret_cast<struct sockaddr*>(&clientAddr), sizeof(clientAddr));
+    if (bindResult < 0) {
+        perror("Error binding ListenSocket");
+        return;
+    }
+
+    printf("Server is waiting for data on port %s...\n", DEFAULT_PORT);
+    listen(ListenSocket, 10);  // Commence à écouter sur le socket
 }
